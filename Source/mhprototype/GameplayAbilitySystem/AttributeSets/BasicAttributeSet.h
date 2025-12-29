60092ceb3de54b1f679eac9a7099aa6de75c98fa
Jun Ming <chongjm@aeonsparx.com>
2025-12-29 17:54:13 +0800

    Added Heal and Damage effect actors and their gameplay effects

--- Source/mhprototype/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
+++ Source/mhprototype/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
@@ -63,4 +63,6 @@ public:
 	}
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 };
