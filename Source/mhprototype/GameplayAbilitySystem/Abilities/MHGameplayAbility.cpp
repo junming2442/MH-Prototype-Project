c98864ba56614e8f486cae81b9db1d154b844943
TencentGo\Junming <v_fjmchong@global.tencent.com>
2026-03-12 17:42:57 +0800

    Enemy abilities WIP

--- Source/mhprototype/GameplayAbilitySystem/Abilities/MHGameplayAbility.cpp
+++ Source/mhprototype/GameplayAbilitySystem/Abilities/MHGameplayAbility.cpp
@@ -8,3 +8,13 @@ UMHGameplayAbility::UMHGameplayAbility()
 	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("GameplayAbility.Active")));
 	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Dead")));
 }
+
+bool UMHGameplayAbility::HasPlayerController() const
+{
+	const APawn* PawnObject = Cast<APawn>(GetAvatarActorFromActorInfo());
+	if (!PawnObject)
+	{
+		return false;
+	}
+	return PawnObject->GetController()->IsA<APlayerController>();
+}
--- Source/mhprototype/GameplayAbilitySystem/Abilities/MHGameplayAbility.h
+++ Source/mhprototype/GameplayAbilitySystem/Abilities/MHGameplayAbility.h
@@ -20,4 +20,8 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	bool ShouldShowInAbilitiesBar = false;
+
+private:
+	UFUNCTION(BlueprintCallable, Category = "Helpers")
+	bool HasPlayerController() const;
 };
